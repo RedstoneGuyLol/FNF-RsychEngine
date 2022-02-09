866cec7eb6d994fbcc43115cd458351ed14b0169
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-02-09 18:56:37 -0500

    Update ExtraSubState.hx
    simple crap

--- source/options/ExtraSubState.hx
+++ source/options/ExtraSubState.hx
@@ -1,1 +1,46 @@
+package options;
 
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class GameplaySettingsSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Extra Settings';
+		rpcTitle = 'Extra Settings Menu'; //for Discord Rich Presence
+		
+		var option:Option = new Option('Controller Mode',
+			'Check this if you want to play with\na controller instead of using your Keyboard.',
+			'controllerMode',
+			'bool',
+			false);
+		addOption(option);
+    
+		super();
+	}
+}
