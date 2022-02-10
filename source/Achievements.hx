6878bc6563a2733bf7a3368d212fbf5df52e93f7
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-02-09 20:40:33 -0500

    Add achviv

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -31,6 +31,7 @@ class Achievements {
 	public static var achievementShits:Array<Dynamic> = [//Name, Description, Achievement save tag, Unlocks after, Hidden achievement
 		//Set unlock after to "null" if it doesnt unlock after a week!!
 		["Freaky on a Friday Night",	"Play on a Friday... Night.",						'friday_night_play',	 null, 			true],
+		["Modified Modder",	"Playing the Game on the Rsych Version of Pysch",						'friday_night_rsych',	 null, 			true],
 		["She Calls Me Daddy Too",		"Beat Week 1 on Hard with no Misses.",				'week1_nomiss',			'week1', 		false],
 		["No More Tricks",				"Beat Week 2 on Hard with no Misses.",				'week2_nomiss',         'week2', 		false],
 		["Call Me The Hitman",			"Beat Week 3 on Hard with no Misses.",				'week3_nomiss',			'week3', 		false],
