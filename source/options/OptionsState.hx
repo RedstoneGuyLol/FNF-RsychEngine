e20dc9803bcd6d1f578caa7826bdf27f3e3377af
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-02-09 20:10:45 -0500

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -49,7 +49,7 @@ class OptionsState extends MusicBeatState
 			case 'Adjust Delay and Combo':
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
 			case 'Rsych':
-				openSubState(new options.RsychState());
+				LoadingState.loadAndSwitchState(new options.RsychState());
 		}
 	}
 
