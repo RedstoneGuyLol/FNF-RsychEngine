1eb406e90d6e5e0771022ee96def287eae93c939
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-02-14 21:44:25 -0500

    Create RsychState.hx

---
+++ source/RsychState.hx
@@ -0,0 +1,57 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.ui.FlxButtonPlus;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import lime.utils.Assets;
+import flixel.system.FlxSound;
+import openfl.utils.Assets as OpenFlAssets;
+import sys.io.File;
+import sys.FileSystem;
+import haxe.Json;
+import haxe.format.JsonParser;
+import openfl.display.BitmapData;
+import flash.geom.Rectangle;
+import flixel.ui.FlxButton;
+import flixel.FlxBasic;
+import sys.io.File;
+/*import haxe.zip.Reader;
+import haxe.zip.Entry;
+import haxe.zip.Uncompress;
+import haxe.zip.Writer;*/
+
+using StringTools;
+
+class RsychState extends MusicBeatState
+{
+	var mods:Array<ModMetadata> = [];
+	static var changedAThing = false;
+	var bg:FlxSprite;
+	var intendedColor:Int;
+	var colorTween:FlxTween;
+
+	override function create()
+	{
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+
+		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
+		bg.screenCenter();
+    
+	}
+}
