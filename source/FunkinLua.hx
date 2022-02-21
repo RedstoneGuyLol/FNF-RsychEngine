7dfb4dcf880a7bef78eb6486cf5977e9bbdf7675
RSGuy <63218795+RedstoneGuyLol@users.noreply.github.com>
2022-02-21 13:21:38 -0500

    d

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -771,7 +771,9 @@ class FunkinLua {
 			else
 				MusicBeatState.switchState(new FreeplayState());
 
-			FlxG.sound.playMusic(Paths.music('freakyMenu'));
+			if (ClientPrefs.classicmode == false) {
+				FlxG.sound.playMusic(Paths.music('freakyMenu'));
+			}
 			PlayState.changedDifficulty = false;
 			PlayState.chartingMode = false;
 			
